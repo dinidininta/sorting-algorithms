61a8d01b7c168aed1d7736291a83ae9f73c9d446
Dininta Isnurthina <dini.dininta@gmail.com>
2021-08-15 17:38:26 +0700

    Refactor Sorting class structure

--- src/Sorting.js
+++ src/Sorting.js
@@ -2,32 +2,34 @@
  * Represent a Sorting algorithms collection
  */
 
-export default class Sorting {
-  static insertionSort(data) {
-    const sortedData = [data[0]];
-    data.slice(1).forEach((datum, index) => {
-      let i = index + 1;
-      const reversedData = [...sortedData].reverse();
-      reversedData.forEach((temp) => {
-        if (temp > datum) {
-          i -= 1;
-        }
-      });
-      sortedData.splice(i, 0, datum);
+const insertionSort = (data) => {
+  const sortedData = [data[0]];
+  data.slice(1).forEach((datum, index) => {
+    let i = index + 1;
+    const reversedData = [...sortedData].reverse();
+    reversedData.forEach((temp) => {
+      if (temp > datum) {
+        i -= 1;
+      }
     });
-    return sortedData;
-  }
+    sortedData.splice(i, 0, datum);
+  });
+  return sortedData;
+};
 
-  // tessssst
-  static selectionSort(data) {
-    const sortedData = [];
-    const copyData = [...data];
-    data.forEach(() => {
-      const temp = Math.min(...copyData);
-      const i = copyData.indexOf(temp);
-      sortedData.push(temp);
-      copyData.splice(i, 1);
-    });
-    return sortedData;
-  }
-}
+const selectionSort = (data) => {
+  const sortedData = [];
+  const copyData = [...data];
+  data.forEach(() => {
+    const temp = Math.min(...copyData);
+    const i = copyData.indexOf(temp);
+    sortedData.push(temp);
+    copyData.splice(i, 1);
+  });
+  return sortedData;
+};
+
+export default {
+  insertionSort,
+  selectionSort
+};
